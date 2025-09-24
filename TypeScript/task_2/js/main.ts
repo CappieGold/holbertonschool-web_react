dc525ce720e471efaccefecf600ccf760fdf22f8
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-09-24 10:16:38 +0000

    feat: task 5, 6,  and 7

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -23,3 +23,49 @@ class Director implements DirectorInterface {
         return "Getting to director tasks";
     }
 }
+
+class Teacher implements TeacherInterface {
+    workFromHome(): string {
+        return "Cannot work from home"
+    }
+
+    getCoffeeBreak(): string {
+        return "Cannot have a break"
+    }
+
+    workTeacherTasks(): string {
+        return "Getting to work"
+    }
+}
+
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === "number" && salary < 500) {
+    return new Teacher();
+  }
+  return new Director();
+}
+
+type Employee = Director | Teacher;
+function isDirector(employee: Employee): employee is Director {
+    return employee instanceof Director
+}
+
+function executeWork(employee: Employee): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  }
+  return employee.workTeacherTasks();
+}
+
+type Subjects = "Math" | "History";
+function teachClass(todayClass: Subjects): string {
+  return todayClass === "Math" ? "Teaching Math" : "Teaching History";
+}
+
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
+console.log(teachClass("Math"));
+console.log(teachClass("History"));
