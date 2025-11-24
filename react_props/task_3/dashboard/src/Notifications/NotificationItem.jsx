d6550c0633b05955de33a7fe29975c34b11cd2f8
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-11-24 10:27:06 +0000

    fix: task 3

--- react_props/task_3/dashboard/src/Notifications/NotificationItem.jsx
+++ react_props/task_3/dashboard/src/Notifications/NotificationItem.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-function NotificationItem({ type = 'default', html = null, value = '' }) {
+function NotificationItem({ type, html, value }) {
   const styles = {
     color: type === 'urgent' ? 'red' : 'blue'
   };
@@ -33,10 +33,4 @@ NotificationItem.propTypes = {
   value: PropTypes.string
 };
 
-NotificationItem.defaultProps = {
-  type: 'default',
-  html: null,
-  value: ''
-};
-
-export default NotificationItem;
+export default NotificationItem;$
