33ba190fa5bea57d0c3086e1a0f9887ca518cb92
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-11-27 11:50:28 +0000

    fix

--- react_styling/task_3/dashboard/src/main.jsx
+++ react_styling/task_3/dashboard/src/main.jsx
@@ -10,6 +10,6 @@ import App from './App/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App isLoggedIn={true} displayDrawer={true} />
+    <App />
   </StrictMode>,
 )
