fe950a66338719eb826550acbfcbe0553fecb506
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-11-28 14:41:31 +0000

    fix: regex trim email

--- react_state/task_1/dashboard/src/Login/Login.jsx
+++ react_state/task_1/dashboard/src/Login/Login.jsx
@@ -25,7 +25,7 @@ class Login extends React.Component {
     if (!email || typeof email !== 'string') return false;
     const trimmedEmail = email.trim();
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return emailRegex.test(email);
+    return emailRegex.test(trimmedEmail);
   }
 
   /**
