35efe1505f6d3c22069ec58ac55ff6fff8726942
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-12-05 13:32:06 +0000

    feat(task3): Update the NotificationItem component

--- react_hooks/task_2/dashboard/src/Notifications/NotificationItem.jsx
+++ react_hooks/task_2/dashboard/src/Notifications/NotificationItem.jsx
@@ -1,54 +1,54 @@
-import React from 'react';
+import { memo } from 'react';
 import PropTypes from 'prop-types';
 
-class NotificationItem extends React.PureComponent {
-  render() {
-    const { type, html, value, markAsRead, id } = this.props;
+function NotificationItem({ 
+  id = 0, 
+  type = 'default', 
+  value = '', 
+  html = undefined, 
+  markAsRead = () => {} 
+}) {
+  const baseClasses = "pl-1 max-[912px]:text-[20px] max-[912px]:w-full max-[912px]:border-b max-[912px]:border-black max-[912px]:p-[10px_8px]";
+  
+  const colorClass = type === 'default' 
+    ? "text-[color:var(--default-notification-item)]" 
+    : "text-[color:var(--urgent-notification-item)]";
 
-    const baseClasses = "pl-1 max-[912px]:text-[20px] max-[912px]:w-full max-[912px]:border-b max-[912px]:border-black max-[912px]:p-[10px_8px]";
-    const colorClass = type === 'default' 
-      ? "text-[color:var(--default-notification-item)]" 
-      : "text-[color:var(--urgent-notification-item)]";
+  const handleClick = () => {
+    markAsRead(id);
+  };
 
-    if (html !== undefined) {
-      return (
-        <li
-          className={`${colorClass} ${baseClasses}`}
-          data-notification-type={type}
-          dangerouslySetInnerHTML={html}
-          onClick={() => markAsRead(id)}
-        />
-      );
-    }
-
+  if (html !== undefined) {
     return (
       <li
         className={`${colorClass} ${baseClasses}`}
         data-notification-type={type}
-        onClick={() => markAsRead(id)}
-      >
-        {value}
-      </li>
+        dangerouslySetInnerHTML={html}
+        onClick={handleClick}
+      />
     );
   }
+
+  return (
+    <li
+      className={`${colorClass} ${baseClasses}`}
+      data-notification-type={type}
+      onClick={handleClick}
+    >
+      {value}
+    </li>
+  );
 }
 
 NotificationItem.propTypes = {
+  id: PropTypes.number,
   type: PropTypes.string,
   html: PropTypes.shape({
     __html: PropTypes.string
   }),
   value: PropTypes.string,
-  markAsRead: PropTypes.func,
-  id: PropTypes.number
+  markAsRead: PropTypes.func
 };
 
-NotificationItem.defaultProps = {
-  type: 'default',
-  html: undefined,
-  value: '',
-  markAsRead: () => {},
-  id: 0
-};
 
-export default NotificationItem;
+export default memo(NotificationItem);
