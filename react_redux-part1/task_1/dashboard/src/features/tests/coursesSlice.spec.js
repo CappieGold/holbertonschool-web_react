20e52671a2739deb5031b113440113b55c2ab2b2
CappieGold <144028326+CappieGold@users.noreply.github.com>
2026-02-06 10:15:59 +0000

    fix: task 3

--- react_redux-part1/task_1/dashboard/src/features/tests/coursesSlice.spec.js
+++ react_redux-part1/task_1/dashboard/src/features/tests/coursesSlice.spec.js
@@ -1,57 +1,51 @@
-import coursesReducer from '../courses/coursesSlice';
-import { fetchCourses } from '../courses/coursesSlice';
+import coursesReducer, { fetchCourses } from '../courses/coursesSlice';
 import { logout } from '../auth/authSlice';
 import mockAxios from 'jest-mock-axios';
-
-afterEach(() => {
-  mockAxios.reset();
-});
 
 describe('coursesSlice', () => {
   const initialState = {
     courses: [],
   };
 
+  afterEach(() => {
+    mockAxios.reset();
+  });
+
   describe('initialState', () => {
-    it('should return the correct initial state by default', () => {
+    it('should return the correct initial state', () => {
       expect(coursesReducer(undefined, { type: 'unknown' })).toEqual(initialState);
     });
   });
 
   describe('fetchCourses', () => {
-    it('should fetch courses data correctly', () => {
-      const courses = [
+    it('should fetch courses data correctly', async () => {
+      const mockCourses = [
         { id: 1, name: 'ES6', credit: 60 },
         { id: 2, name: 'Webpack', credit: 20 },
-        { id: 3, name: 'React', credit: 40 },
+        { id: 3, name: 'React', credit: 40 }
       ];
 
-      const action = {
-        type: fetchCourses.fulfilled.type,
-        payload: courses,
-      };
-
+      const action = fetchCourses.fulfilled(mockCourses, 'courses/fetchCourses');
       const newState = coursesReducer(initialState, action);
 
-      expect(newState.courses).toEqual(courses);
-      expect(newState.courses).toHaveLength(3);
+      expect(newState.courses).toEqual(mockCourses);
     });
   });
 
   describe('logout action', () => {
-    it('should reset the courses array to empty when logout action is dispatched', () => {
+    it('should reset the courses array to empty whenever the logout action is dispatched', () => {
       const stateWithCourses = {
         courses: [
           { id: 1, name: 'ES6', credit: 60 },
           { id: 2, name: 'Webpack', credit: 20 },
-          { id: 3, name: 'React', credit: 40 },
+          { id: 3, name: 'React', credit: 40 }
         ],
       };
 
-      const newState = coursesReducer(stateWithCourses, logout());
+      const action = logout();
+      const newState = coursesReducer(stateWithCourses, action);
 
       expect(newState.courses).toEqual([]);
-      expect(newState.courses).toHaveLength(0);
     });
   });
 });
